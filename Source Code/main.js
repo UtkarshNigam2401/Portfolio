4d85be33b710ab26946d61c204635f96ea990ce1
Utkarsh-Nigam <utkarsh02n@gmail.com>
2024-02-20 20:22:23 +0530

    Portfolio

---
+++ Source Code/main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Frontend Developer", "Java Developer", "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
